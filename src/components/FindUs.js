b015f2e2be9c44c31cadb14cec9f88f21f4db4d9
Aleksandar <filipovskia@yahoo.com>
2022-12-12 16:50:34 +0100

    findus od firestore

--- src/components/FindUs.js
+++ src/components/FindUs.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import {React, useState} from 'react'
 import FindUsCard from './FindUsCard'
+import { collection, getDocs, getFirestore, doc } from '@firebase/firestore'
+
+const getFromFS = async (setLocations) => {
+  let db = getFirestore();
+  const docRef = collection(db,"restaurants");
+  const docSnap = await getDocs(docRef);
+  
+  let processedData = [];
+  docSnap.forEach(entry => processedData.push({id:entry.id, ...entry.data()}));
+  setLocations(processedData);
+}
 
 const FindUs = () => {
-  const locations = [
-    {id: 1, name:'Debar Maalo', address:'132 Orce Nikolov Str.'},
-    {id: 2, name:'Aerodrom', address:'16 Vidoe Smilevski Bato Str.'}
-  ]
+  const [locations, setLocations] = useState([]);
+  
+  getFromFS(setLocations);
+  
   return (
     <div className='FindUs'>
         <h1>Find Us</h1>
         <p>Our locations:</p>
         {locations.map(l => 
-          <FindUsCard id={l.id} name={l.name} address={l.address}/>
+          <FindUsCard key={l.id} name={l.name} address={l.address}/>
           )}
     </div>
   )
