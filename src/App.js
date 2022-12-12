fbe243828415ab697174722d40b0cd14854f718d
Aleksandar <filipovskia@yahoo.com>
2022-12-12 15:06:26 +0100

    firebase init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home.js';
 import FindUs from './components/FindUs.js';
--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './css/index.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_OeG5MmNul_6QhTtB_16zvamik9uBD2c",
+  authDomain: "crispies-3dcb9.firebaseapp.com",
+  projectId: "crispies-3dcb9",
+  storageBucket: "crispies-3dcb9.appspot.com",
+  messagingSenderId: "1075287717682",
+  appId: "1:1075287717682:web:805edd9097efe41635892b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
