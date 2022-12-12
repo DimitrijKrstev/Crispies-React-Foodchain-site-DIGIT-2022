a672c0bb21f085f2a507559e3ae780475f08dc2f
DimitrijKrstev <antishadowss@gmail.com>
2022-12-12 16:48:18 +0100

    db export in index + signin started

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { auth, db } from '../index';
+import { useState } from 'react';
+import { doc, getDoc, setDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import '../css/SignIn.css';
 
-const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
+async function LogIn(email, password, changeUser) {
+    if (email && password) {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            if (auth) {
+                const user = (await getDoc(doc(db, 'users', auth.currentUser.uid))).data();
+                changeUser(user);
+              }
+        }
+        catch (error) {
+            document.getElementById("errorP").innerHTML = (error.code);
+        }
+    }
+    else {
+        document.getElementById("errorP").innerHTML = "Enter both an email and a password.";
+    }
 }
 
-export default SignIn
+    const SignIn = (props) => {
+        const [state, changeState] = useState("initial")
+        return (
+            <div className='flex flex-col items-center' id ="SignContainer">
+                {auth.currentUser && <div><p>Logged in as {auth.currentUser.uid}</p><button>Log out</button></div>}
+                {state === "initial" &&
+                    <div>
+                        <button onClick={() => changeState("LogIn")}>Log in</button>
+                        <button onClick={() => changeState("SignUp")}>Sign up</button>
+                    </div>}
+                {state === "LogIn" &&
+                    <div className='flex flex-col'>
+                        <label>Email:</label><input type="text"></input>
+                        <label>Password:</label><input type="password"></input>
+                        <button onClick={() => changeState("SignUp")}>Sign up instead</button>
+                    </div>}
+                {state === "SignUp" &&
+                    <div className='flex flex-col'>
+                        <label>Email:</label><input type="text"></input>
+                        <label>Username:</label><input type="text"></input>
+                        <label>Password:</label><input type="password"></input>
+                        <button onClick={() => changeState("LogIn")}>Log in instead</button>
+                    </div>}
+            </div>
+        )
+    }
+
+    export default SignIn
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './css/index.css';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,6 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
