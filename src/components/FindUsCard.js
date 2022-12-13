c77978027d460bf758635a177d4d76333fa222a4
Aleksandar <filipovskia@yahoo.com>
2022-12-13 14:20:53 +0100

    FindUs basic functionality

--- src/components/FindUsCard.js
+++ src/components/FindUsCard.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const FindUsCard = ({id, name, address}) => {
+const FindUsCard = ({id, name, address, imgUrl, showOnMap}) => {
+  const [mapOpen, setMapOpen] = useState(false);
+
   return (
-    <div id={id}>
+    <div id={id} className="FindUsCard flex grow flex-col items-center">
         <h3>{name}</h3>
         <p>{address}</p>
+        <button onClick={() => {
+          if (window.innerWidth <= 850)
+            setMapOpen(!mapOpen);
+          showOnMap(imgUrl)
+          }}>
+            <span className="mapButtonHorizontal">Map</span>
+            {!mapOpen && <span className="mapButtonVerticalShow">Show Map</span>}
+            {mapOpen && <span className="mapButtonVerticalHide">Hide Map</span>}
+        </button>
+        {mapOpen && <img className="verticalMap" src={imgUrl}/>}
     </div>
   )
 }
 
-export default FindUsCard
+export default FindUsCard;
