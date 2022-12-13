3143a386c90cb207cde99a59a5778327797d0ca9
Luka Krstikj <luka.krstik@protonmail.com>
2022-12-13 14:26:32 +0100

    ikoni vo navbar sredeni tip top sega

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "../fonts/FredokaOne-Regular.ttf"
 import "../css/Navbar.css"
 import accPic from '../images/accJSX.svg';
+import cartPic from '../images/cart.svg';
 
 const Navbar = () => {
     return (
@@ -13,29 +14,13 @@ const Navbar = () => {
                 <button>
                     <h2 className="bg-black hover:bg-black-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500">Find Us</h2>
                 </button>
-                <div className="buttonBorder">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fillRule="evenodd"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeMiterlimit="1.5"
-                            clipRule="evenodd"
-                            viewBox="0 0 600 600"
-                            className="float-right w-10 pt-1 mr-1  justify-end"
-                        >
-                            <path fill="none" d="M0 0H600V600H0z"></path>
-                            <clipPath id="_clip1">
-                                <path d="M0 0H600V600H0z"></path>
-                            </clipPath>
-                            <g fill="none" stroke="#000" strokeWidth="25" clipPath="url(#_clip1)">
-                                <path d="M587 13l-87 30-50 457H150"></path>
-                                <circle cx="450" cy="544.5" r="44.5"></circle>
-                                <circle cx="150" cy="544.5" r="44.5"></circle>
-                                <path d="M458 420H13V100h476l-31 320zM13 340h453.267M13 256.5h462M13 180h468M120 100v320M223 100v320M326 100v320M429 100v320"></path>
-                            </g>
-                        </svg>
-                    <img src={accPic} className="float-right w-10 pt-1 mx-5 justify-end"></img>
+                <div className="flex flex-row">
+                    <div className="buttonBorder">
+                        <img src={accPic} className="float-right w-10 pt-1 mx-3 justify-end"></img>
+                    </div>
+                    <div className="buttonBorder">
+                        <img src={cartPic} className="float-right w-10 pt-1 mx-3 justify-end"></img>
+                    </div>
                 </div>
             </div>
         </div>
