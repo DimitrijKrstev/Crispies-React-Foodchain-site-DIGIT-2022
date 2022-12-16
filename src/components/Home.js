30f50e4fb72e30c443730885f03f1d56573fa2fd
DimitrijKrstev <antishadowss@gmail.com>
2022-12-16 12:38:57 +0100

    Carousel clickable + css tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,7 +28,7 @@ export default function Home() {
     <h1 class=" border-b-2 border-offblack text-4xl text-center bg-beige titleLocations py-3">Your Taste buds' second home</h1>
     <div className="flex justify-center">
       <div className="flex flex-col items-center bg-beigeLight border-2 border-black mb-10 mt-8 min-w-[21rem]">
-        <Swiper
+        <Link to="Menu"><Swiper
           spaceBetween={0}
           centeredSlides={true}
           autoplay={{
@@ -48,7 +48,7 @@ export default function Home() {
         >
           <SwiperSlide className="flex justify-center"><img src={carousel1}></img></SwiperSlide>
           <SwiperSlide className="flex justify-center"><img src={carousel2}></img></SwiperSlide>
-        </Swiper>
+        </Swiper></Link>
         <h1 className="font-bold mt-2 text-3xl textSignin mt-8 mb-4">Specials</h1>
         <div className="flex justify-center mb-5 flex-wrap max-w-[70vw]">
           <HomeMenuCard name="Crispy Chicken Combo" price="360" picture="https://media.discordapp.net/attachments/441534031016755200/1052968276382199891/Combo_Cheeseburger.jpg"></HomeMenuCard>
